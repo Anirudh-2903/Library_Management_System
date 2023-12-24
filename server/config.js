7267df1c57d541d142521ebe250ef9af67633c33
Your Name <you@example.com>
2023-12-24 08:49:34 +0530

    database initial setup

--- server/config.js
+++ server/config.js
@@ -1,1 +1,3 @@
 export const port = 5913;
+
+export const mongoDBURL = 'mongodb+srv://main:7i9lD6slEC2RdrGt@mern-lib-mng.ql7bgiz.mongodb.net/?retryWrites=true&w=majority'
--- server/index.js
+++ server/index.js
@@ -2,14 +2,52 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import { port } from './config.js';
+import { Book } from './models/book.js';
+import { port , mongoDBURL } from './config.js';
+
 
 const app = express()
 
 app.get('/' , (req,res) => {
-    res.send('Hello World')
+    console.log(req)
+    return res.status(234).send('Welcome to the Project')
 })
 
-app.listen( port , () => {
-    console.log(`App listening on port ${port}`)
-})
+// create a new book
+app.post('/books',(req,res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.bookID ||
+            !req.body.author ||
+            !req.body.published  ) {
+
+                return res.status(400).send({
+                    message : 'Send all required fields : title , bookID , author , published',
+                });
+        }
+        const newBook = {
+            title : req.body.title,
+            bookID : req.body.bookID,
+            author : req.body.author,
+            published : req.body.published,
+        };
+        const book =  Book.create(newBook);
+        return res.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).senc({message : error.message});
+    }
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen( port , () => {
+            console.log(`App listening on port : ${port}`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
