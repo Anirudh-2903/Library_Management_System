3e7c668f858ca92c8dd1a4a2d038b21ccea552e7
Anirudh <anirudh.das2903@outlook.in>
2024-01-07 17:20:40 +0530

    deploy build

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3001'});
+const API = axios.create({ baseURL: 'https://upset-lion-garters.cyclic.app'});
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
